9c1cb1ed4b7933e2d6b3f568bc7c65f8d21f864d
Arthur Badu Reis <143804510+Atu141@users.noreply.github.com>
2025-03-17 12:00:43 -0300

    Delete Cp1 directory

--- Cp1/src/main/java/com/github/Atu141/Cp1/service/execeptions/DatabaseException.java
+++
@@ -1,7 +0,0 @@
-package com.github.Atu141.Cp1.service.execeptions;
-
-public class DatabaseException extends RuntimeException {
-    public DatabaseException(String message) {
-        super(message);
-    }
-}
